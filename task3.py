ea3a4562c7c57e56b70c93a304bc82cc1f4bf190
Timur Tursunov <t.timon2003@gmail.com>
2021-11-13 19:54:25 +0500

    solved task for AND and OR operators

---
+++ task3.py
@@ -0,0 +1,20 @@
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+
+# print(f'{num1} + {num2} = {num1 + num2}')
+# print(f'{num1} - {num2} = {num1 - num2}')
+# print(f'{num1} * {num2} = {num1 * num2}')
+# print(f'{num1} / {num2} = {num1 / num2}')
+
+
+
+# print(True and False)
+
+
+
+login = 'admin'
+password = 'admin'
+
+if login == 'admin' and password == 'admin': print('Welcome')
+else: print('Bye') 
+
